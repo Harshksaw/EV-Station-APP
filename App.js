9f930c62ca3617c8d5713e1abf1fd75725c507d2
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-01-09 16:37:37 +0530

    Map Added with marker

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
@@ -7,9 +7,13 @@ import LoginScreen from './App/Screen/LoginScreen/LoginScreen';
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import * as SecureStore from "expo-secure-store";
 
+import * as Location from 'expo-location';
+
 
 import { NavigationContainer } from '@react-navigation/native';
 import MyTabs from "./App/Navigation/TabNavigation";
+import { UserLocationContext } from './App/Context/UserLocationContext';
+
 
 
 const tokenCache = {
@@ -33,6 +37,33 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
 
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      console.log("loc", location)
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+
   const [fontsLoaded] = useFonts({
     'Inter-Black': require('./assets/fonts/RethinkSans-VariableFont_wght.ttf'),
     'Inter-Italic': require('./assets/fonts/RethinkSans-Italic-VariableFont_wght.ttf'),
@@ -53,24 +84,27 @@ export default function App() {
     <ClerkProvider
       tokenCache={tokenCache}
       publishableKey={'pk_test_bW9kZXJuLWNvbGxpZS05LmNsZXJrLmFjY291bnRzLmRldiQ'}>
-      <View style={styles.container} onLayout={onLayoutRootView}>
 
-        <SignedIn>
+      <UserLocationContext.Provider value={{location}}  >
+        <View style={styles.container} onLayout={onLayoutRootView}>
 
-
-          <MyTabs />
-
-
-        </SignedIn>
-
-        <SignedOut>
-
-          <LoginScreen />
-        </SignedOut>
+          <SignedIn>
 
 
-        <StatusBar style="auto" />
-      </View>
+            <MyTabs />
+
+
+          </SignedIn>
+
+          <SignedOut>
+
+            <LoginScreen />
+          </SignedOut>
+
+
+          <StatusBar style="auto" />
+        </View>
+      </UserLocationContext.Provider>
     </ClerkProvider>
   );
 }
--- App/Screen/HomeScreen/AppMapView.jsx
+++ App/Screen/HomeScreen/AppMapView.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useContext } from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import { Image, StyleSheet, View } from 'react-native';
 import MapViewStyle from '../../utils/MapViewStyle.json'
+import { UserLocationContext } from '../../Context/UserLocationContext';
 
 
 function AppMapView() {
-    console.log("Appmapview")
-    return (
+
+    const {location , setLocation} = useContext(UserLocationContext);
+
+    console.log("loc", location)
+
+
+
+    return location?.latitude && (
+      <View>
 
         <MapView style={styles.map} 
         customMapStyle={MapViewStyle}
+        region={{
+          latitude:location?.latitude,
+          longitude:location?.longitude,
+          latitudeDelta:0.0422,
+          longitudeDelta:0.0421,
+        }}
+        
 
-        />
+
+        >
+        <Marker
+        coordinate={{
+
+          latitude:location?.latitude,
+          longitude:location?.longitude,
+        }}
+
+        >
+          <Image source={require('../../../assets/images/car.png')} style={{width:60 , height:60}} />
+        </Marker>
+        </MapView>
+
+        </View>
 
     );
   }
